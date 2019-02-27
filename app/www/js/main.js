5233f9ecebf2c04856eaee2a4cdf972ba73e7eec
Roshan Ravi <roshanravi10@gmail.com>
2019-02-26 21:45:32 -0500

    Disable arrows on number input

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -84,6 +84,24 @@ $(function () {
     $("button[data-type='plus']").click((event) => {
         addCounterValue(event.currentTarget, +1);
     });
+    
+    // Disable scroll when focused on a number input.
+    $('form').on('focus', 'input[type=number]', function(e) {
+        $(this).on('wheel', function(e) {
+            e.preventDefault();
+        });
+    });
+ 
+    // Restore scroll on number inputs.
+    $('form').on('blur', 'input[type=number]', function(e) {
+        $(this).off('wheel');
+    });
+ 
+    // Disable up and down keys.
+    $('form').on('keydown', 'input[type=number]', function(e) {
+        if ( e.which == 38 || e.which == 40 )
+            e.preventDefault();
+    });  
 });
 
 $(applicationCache)
